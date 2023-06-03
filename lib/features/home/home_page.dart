7b680d76631e59924b1cb6f30d84522a735db864
Tarun Tanmay <90436309+taruntanmay730@users.noreply.github.com>
2023-06-03 20:24:26 +0530

    first commit

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+
         color: Colors.pinkAccent,
         child: const Text("Home"),
       ),
